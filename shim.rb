5c1c41701ae9876b197d7c07e443e24e02e0e557
Sterling Radcliff <radcliff@riseup.net>
2015-07-30 07:08:35 -0700

    add /status endpoint and check for valid channel key

--- shim.rb
+++ shim.rb
@@ -1,8 +1,24 @@
 class Shim < Sinatra::Base
   register Sinatra::Contrib
 
-  get '/' do
-    status 200
-    body "unofficial earthsky tonight ifttt shim"
+  configure do
+    set :IFTTT_CHANNEL_KEY, ENV['IFTTT_CHANNEL_KEY']
   end
+
+  namespace '/ifttt' do
+    namespace '/v1' do
+
+      before do
+        unless env['HTTP_IFTTT_CHANNEL_KEY'] == settings.IFTTT_CHANNEL_KEY
+          halt 401, { 'Content-Type': 'text/plain' }, 'Invalid IFTTT channel key!'
+        end
+      end
+
+      get '/status' do
+        status 200
+      end
+
+    end
+  end
+
 end
--- spec/shim_spec.rb
+++ spec/shim_spec.rb
@@ -2,19 +2,25 @@ require_relative "spec_helper"
 require_relative "../shim.rb"
 
 describe Shim do
+  include Rack::Test::Methods
 
   def app
     Shim
   end
 
-  it 'works' do
-    get '/'
-    expect(last_response).to be_ok
-  end
-
   describe 'conformms to IFTTT channel protocol' do
-    xit 'works' do
-    end 
+    context 'a /status endpoint' do
+      it 'exists' do
+        get '/ifttt/v1/status', nil, { IFTTT_CHANNEL_KEY: ENV['IFTTT_CHANNEL_KEY'] }
+        expect(last_response.status).to eq(200)
+      end
+
+      it 'returns 401 with an invalid channel key' do
+        get '/ifttt/v1/status', nil, { IFTTT_CHANNEL_KEY: 'INVALID' }
+        expect(last_response.status).to eq(401)
+      end
+
+    end
   end
 
 end
