bea7292ee09cf350509c1d9337fd9720d1c25f80
Sterling Radcliff <radcliff@riseup.net>
2015-07-30 14:32:24 -0700

    initial tests for ifttt channel protocol reqs

--- spec/shim_spec.rb
+++ spec/shim_spec.rb
@@ -8,7 +8,7 @@ describe Shim do
     Shim
   end
 
-  describe 'conformms to IFTTT channel protocol' do
+  describe 'conformms to IFTTT channel protocol:' do
     context 'a /status endpoint' do
       it 'exists' do
         get '/ifttt/v1/status', nil, { IFTTT_CHANNEL_KEY: ENV['IFTTT_CHANNEL_KEY'] }
@@ -19,6 +19,14 @@ describe Shim do
         get '/ifttt/v1/status', nil, { IFTTT_CHANNEL_KEY: 'INVALID' }
         expect(last_response.status).to eq(401)
       end
+    end
+
+    context 'a /test/setup endpoint' do
+      it 'exists' do
+        post '/test/setup', nil, { IFTTT_CHANNEL_KEY: ENV['IFTTT_CHANNEL_KEY'] }
+        expect(last_response.status).to eq(200)
+      end
+    end
 
     end
   end
